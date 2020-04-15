6706fcbff245a0f9cc8eb53cfe971fc0a2ad8ce6
Ada Lovelace <ada@lovelace.com>
2020-04-15 18:54:45 +0400

    Removes unused imports

--- initial/src/main/java/com/playground/observability/german/GermanProcessingLauncher.java
+++ initial/src/main/java/com/playground/observability/german/GermanProcessingLauncher.java
@@ -4,15 +4,11 @@ import brave.http.HttpTracing;
 import brave.spring.web.TracingClientHttpRequestInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 @SpringBootApplication(scanBasePackages = {"com.playground.observability.german", "com.playground.observability.common"})
 public class GermanProcessingLauncher {
