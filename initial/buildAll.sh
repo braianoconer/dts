58655cfb58102aca05e3fbb94d4093fead0e0c51
Ada Lovelace <ada@lovelace.com>
2020-04-23 15:17:46 +0400

    Changes the ame of images to include dts prefix

--- initial/buildAll.sh
+++ initial/buildAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DOCKER_BUILDKIT=1 docker build -f English-Dockerfile -t observability/english .
-DOCKER_BUILDKIT=1 docker build -f Translator-Dockerfile -t observability/translator .
-DOCKER_BUILDKIT=1 docker build -f Spanish-Dockerfile -t observability/spanish .
-DOCKER_BUILDKIT=1 docker build -f Italian-Dockerfile -t observability/italian .
-DOCKER_BUILDKIT=1 docker build -f German-Dockerfile -t observability/german .
+DOCKER_BUILDKIT=1 docker build -f English-Dockerfile -t dts/english .
+DOCKER_BUILDKIT=1 docker build -f Translator-Dockerfile -t dts/translator .
+DOCKER_BUILDKIT=1 docker build -f Spanish-Dockerfile -t dts/spanish .
+DOCKER_BUILDKIT=1 docker build -f Italian-Dockerfile -t dts/italian .
+DOCKER_BUILDKIT=1 docker build -f German-Dockerfile -t dts/german .
